ba91d1766df68a5f00365820cb494a3c615db087
nayeem78 <khan.nayeem78@gmail.com>
2016-10-20 10:04:16 +0200

    Lab 1
    DSP Exercise

---
+++ Lab1/Matlab/ramp.m
@@ -0,0 +1,15 @@
+function  x = ramp( n, N )
+%DIRAC Summary of this function goes here
+%   Detailed explanation goes here
+if (n>(N-1) |n<1)
+    disp ('Values are:')
+    
+else
+    array = zeros (1,N);
+
+    array(n:N)=1;
+    x= array;
+end
+
+
+end
